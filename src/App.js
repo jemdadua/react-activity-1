9ebfc875ea504463c1ff8e82a5f54bb943be9965
Jeremy Dadua <jeremy-amadeo.dadua1@trans-cosmos.co.jp>
2023-06-13 10:02:36 +0800

    asdasd

--- src/App.js
+++ src/App.js
@@ -1,66 +1,14 @@
-import logo from './logo.svg';
-import React, { useState , useEffect} from 'react';
+import React from 'react';
 import './App.css';
+import Temperature from './component/Temperature';
 
 
 
-
-
-function App() {
-  const [temperature, setTemperature] = useState(0);
-  const [tempColor, setTempColor] = useState("zero");
-
-  useEffect(() => {
-
-    switch(true){
-      case temperature <= -1:
-        setTempColor("lessfive");
-        break;
-      case temperature <= 14:
-        setTempColor("zero");
-        break;
-      case temperature <= 29:
-        setTempColor("fifteen");
-        break;
-      case temperature <= 44:
-        setTempColor("thirty");
-        break;
-      case temperature >= 45:
-        setTempColor("fourty");
-        break;
-    }
-  }, [temperature] );
-
-  const tempIncrease = () =>{
-    setTemperature(temperature + 1);
-     if(temperature == 50){
-      setTemperature(temperature);
-    }
-  };
-
-  const tempDecrease = () =>{
-    setTemperature(temperature - 1);
-    if(temperature == -10){
-      setTemperature(temperature);
-    }
-  };
-
-  return (
-    <div className='container'>
-      <div className='appContainer'>
-        <div className={`appDisplay ${tempColor}`}>
-         {temperature}°C
-        </div>
-        <div className='appBtn'>
-          <button className='subBtn' onClick={tempDecrease}>-</button>
-          <button className='addBtn' onClick={tempIncrease}>+</button>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-
+const App = () => (
+  <div className='container'>
+    <Temperature/>
+  </div>
+)
 
 
 export default App;
